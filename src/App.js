217194c6153639fe6019147d0cc12ea84372e32f
Piron-Johny <piron.johny@gmail.comm>
2022-04-10 20:57:25 +0300

    fix pach 2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Suspense fallback={<div>Liading...</div>}>
       <Routes>
-        <Route path="goit-react-hw-05-movies/" element={<Layout />}>
+        <Route path="/*" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="movies" element={<MoviesPage />}>
             <Route path=":moviesId" element={<MoviesDitailPage />}>
